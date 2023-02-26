aa2e36c202c900201aa194ee5cf94af89e86a576
63411 <62799417+molneya@users.noreply.github.com>
2023-02-27 01:03:42 +0800

    Update overallStrain only when all concurrent notes have been processed

--- osu.Game.Rulesets.Mania/Difficulty/Skills/Strain.cs
+++ osu.Game.Rulesets.Mania/Difficulty/Skills/Strain.cs
@@ -25,6 +25,10 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Skills
         private readonly double[] endTimes;
         private readonly double[] individualStrains;
 
+        private double holdFactor; // Factor to all additional strains in case something else is held
+        private double holdAddition; // Addition to the current note in case it's a hold and has to be released awkwardly
+        private int chordSize; // Count of how many concurrent notes there are
+
         private double individualStrain;
         private double overallStrain;
 
@@ -35,27 +39,31 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Skills
             endTimes = new double[totalColumns];
             individualStrains = new double[totalColumns];
             overallStrain = 1;
+
+            holdFactor = 1;
+            holdAddition = 0;
+            chordSize = 0;
         }
 
         protected override double StrainValueOf(DifficultyHitObject current)
         {
             var maniaCurrent = (ManiaDifficultyHitObject)current;
+            var maniaNext = (ManiaDifficultyHitObject)current.Next(0);
             double startTime = maniaCurrent.StartTime;
             double endTime = maniaCurrent.EndTime;
             int column = maniaCurrent.BaseObject.Column;
             bool isOverlapping = false;
-
             double closestEndTime = Math.Abs(endTime - startTime); // Lowest value we can assume with the current information
-            double holdFactor = 1.0; // Factor to all additional strains in case something else is held
-            double holdAddition = 0; // Addition to the current note in case it's a hold and has to be released awkwardly
+            chordSize++;
 
             for (int i = 0; i < endTimes.Length; ++i)
             {
                 // The current note is overlapped if a previous note or end is overlapping the current note body
-                isOverlapping |= Precision.DefinitelyBigger(endTimes[i], startTime, 1) && Precision.DefinitelyBigger(endTime, endTimes[i], 1);
+                if (Precision.DefinitelyBigger(endTimes[i], startTime, 1) && Precision.DefinitelyBigger(endTime, endTimes[i], 1))
+                    isOverlapping = true;
 
                 // We give a slight bonus to everything if something is held meanwhile
-                if (Precision.DefinitelyBigger(endTimes[i], endTime, 1))
+                if (Precision.DefinitelyBigger(endTimes[i], endTime, 1) && !Precision.AlmostEquals(startTimes[i], startTime, 1))
                     holdFactor = 1.25;
 
                 closestEndTime = Math.Min(closestEndTime, Math.Abs(endTime - endTimes[i]));
@@ -72,22 +80,29 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Skills
             // 0.0 +--------+-+---------------> Release Difference / ms
             //         release_threshold
             if (isOverlapping)
-                holdAddition = 1 / (1 + Math.Exp(0.5 * (release_threshold - closestEndTime)));
+                holdAddition += 1 / (1 + Math.Exp(0.5 * (release_threshold - closestEndTime)));
 
             // Decay and increase individualStrains in own column
+            overallStrain = applyDecay(overallStrain, current.DeltaTime, overall_decay_base);
             individualStrains[column] = applyDecay(individualStrains[column], startTime - startTimes[column], individual_decay_base);
             individualStrains[column] += 2.0 * holdFactor;
 
             // For notes at the same time (in a chord), the individualStrain should be the hardest individualStrain out of those columns
             individualStrain = maniaCurrent.DeltaTime <= 1 ? Math.Max(individualStrain, individualStrains[column]) : individualStrains[column];
 
-            // Decay and increase overallStrain
-            overallStrain = applyDecay(overallStrain, current.DeltaTime, overall_decay_base);
-            overallStrain += (1 + holdAddition) * holdFactor;
-
             // Update startTimes and endTimes arrays
             startTimes[column] = startTime;
             endTimes[column] = endTime;
+
+            // We are still adding strain at the current time so there is no need to return difficulty just yet
+            if ((maniaNext != null) && (Precision.AlmostEquals(maniaNext.DeltaTime, 0, 1)))
+                return 0;
+
+            // Now that we have accounted for all concurrent notes, we can add to the overallStrain
+            overallStrain += (chordSize + holdAddition) * holdFactor;
+            holdFactor = 1;
+            holdAddition = 0;
+            chordSize = 0;
 
             // By subtracting CurrentStrain, this skill effectively only considers the maximum strain of any one hitobject within each strain section.
             return individualStrain + overallStrain - CurrentStrain;
